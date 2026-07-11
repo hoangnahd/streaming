b52ff2b8faca6b25003b3a664ad93dc24b1f9466
hoangnahd <135134713+hoangnahd@users.noreply.github.com>
2026-07-11 13:29:13 +0000

    save current state

--- src/main/java/com/app/streaming/model/SessionRegistry.java
+++ src/main/java/com/app/streaming/model/SessionRegistry.java
@@ -1,45 +1,27 @@
 package com.app.streaming.model;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.WebSocketSession;
 
 import java.util.HashMap;
-import java.util.List;
+
 @Component
 public class SessionRegistry {
 
     private HashMap<String, StreamingClient> clients = new HashMap<>();
 
-    public List<StreamingClient> getClientsByRoomId(String roomId) {
-        return clients.values().stream().filter(
-            client -> client.getRoomId().equals(roomId)
-        ).toList();
-    }
-
-    public StreamingClient getClientById(String sessionId) throws Exception {
-        StreamingClient client = clients.get(sessionId);
-        if(client == null) {
-            throw new Exception("[Handler] Session haven't registered");
-        }
-        if(!client.isOpen()) {
-            throw new Exception("[Handler] Session is not alive");
-        }
-
-        return clients.get(sessionId);
-    }
     // Initialize header
     public boolean isSessionAlive(String sessionId) {
         StreamingClient client = clients.get(sessionId);
         if(client != null && !client.isOpen()) {
-            removeSession(client.getSessionId());
+            removeSession(client.getId());
             return false;
         }
         return true;
     }
     
     public void registerClient(StreamingClient client) {
-        if(clients.get(client.getSessionId()) == null && client.isOpen()) {
-            clients.put(client.getSessionId(), client);
+        if(clients.get(client.getId()) == null && client.isOpen()) {
+            clients.put(client.getId(), client);
             System.out.println("[Session registry]: Session registered successfully");
         } else
             System.out.println("[Session registry]: Session existed or dead!");
@@ -49,23 +31,7 @@ public class SessionRegistry {
         clients.remove(sessionId);
     }
 
-    public void updateInitHeader(String sessionId, byte[] initHeader) {
-        StreamingClient client = clients.get(sessionId);
-        if (client != null) {
-            client.setInitializationHeaderSegment(initHeader);
-        }
-    }
-
-    public List<WebSocketSession> getSessions(String roomId) {
-        return this.clients.values().stream()
-            .filter(client -> {
-                return client != null && client.getRoomId().equals(roomId);
-            }).map(StreamingClient::getSession).toList();
-    }
-
-    public List<byte[]> getInitHeaders(String roomId) {
-        return this.clients.values().stream().filter(client -> {
-            return client != null && client.getRoomId().equals(roomId);
-        }).map(StreamingClient::getInitializationHeaderSegment).toList();
+    public StreamingClient getClientById(String id) {
+        return clients.get(id);
     }
 }
